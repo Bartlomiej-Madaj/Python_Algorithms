a827689d84714741c2a504de2f66ce2578bdb3fe
Bartek <bmadej2503@gmail.com>
2024-03-06 08:49:42 +0100

    Add selection sort.

--- basic_sort.py
+++ basic_sort.py
@@ -7,21 +7,19 @@ class BasicSort:
 
         for i in range(len(self.array)):
             index = len(self.array) - i
-            for j in range(1, index):
-                if self.array[j-1] > self.array[j]:
-                    self._swap(j-1, j)
+            for j in range(index-1):
+                if self.array[j] > self.array[j+1]:
+                    self._swap(j, j+1)
                 
-        
-        # for i in range(len(self.array)):
-        #     index = len(self.array) - i
-        #     max_value_index = 0
-        #     for j in range(0, index):
-        #         if self.array[j] > self.array[max_value_index]:
-        #             max_value_index = j
-            
-        #     temp = self.array[max_value_index]
-        #     self.array[max_value_index] = self.array[index-1]
-        #     self.array[index-1] = temp
+    def selection_sort(self):
+        for i in range(len(self.array)):
+            index = len(self.array)
+            min_value_index = i 
+            for j in range(i, index):
+                if self.array[j] < self.array[min_value_index]:
+                    min_value_index = j
+            if i != min_value_index:
+                self._swap(i, min_value_index)
 
     def _swap(self, index1, index2):
         temp = self.array[index1]
@@ -34,6 +32,7 @@ if __name__ == '__main__':
 
     sort = BasicSort(test)
 
-    sort.bubble_sort()
+    # sort.bubble_sort()
+    sort.selection_sort()
 
     print(test)
